524f74575162b0439f9c248a382782ac30d47da4
Andrei Alexeenco <andrei.alexeenco@gmail.com>
2021-10-09 21:09:16 +0300

    chore: cleanup commented code
    Cleanup commented code.

--- src/js/content.js
+++ src/js/content.js
@@ -1,14 +1,7 @@
 (async () => {
-    //const { nodeAddedAsync } = await import(
-    //    chrome.runtime.getURL("js/modules/utils.mjs")
-    //);
     const { YTChannelPlayer, YTMainPlayer } = await import(
         chrome.runtime.getURL("js/modules/player.mjs")
     );
-    //const containerId = "columns";
-    //if (!document.getElementById(containerId)) {
-    //    await nodeAddedAsync(document, (node) => node.id === containerId)
-    //}
     new YTChannelPlayer().initAsync();
     new YTMainPlayer().initAsync();
 })();
