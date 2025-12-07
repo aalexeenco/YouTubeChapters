2ff3892b8f5a0dfaea9c28eb371422d99198ad23
Andrei Alexeenco <andrei.alexeenco@gmail.com>
2025-12-07 18:53:55 +0200

    Update dev deps.

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "prettier"),
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+            chrome: "readonly",
+        },
+
+        parser: babelParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {},
+}]);
